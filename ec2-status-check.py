ae903b579c88001698500cfed3f84e93deb07b25
Leandro Mileski <leandrotinoco55@gmail.com>
2026-05-04 17:27:41 +0100

    add: ec2-status-check.py

---
+++ ec2-status-check.py
@@ -0,0 +1,14 @@
+import boto3
+
+client = boto3.client('ec2', region_name = "eu-north-1")
+
+response = client.describe_instance_status(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+if response:
+    print("Status   ID")
+    for inst in response['InstanceStatuses']:
+        print(f"{inst['InstanceState']['Name']} {inst['InstanceId']}")
+
+print('----------')
+
