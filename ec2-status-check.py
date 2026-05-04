a561ffb9831055d8226decae3ca57a6bdf161c43
Leandro Mileski <leandrotinoco55@gmail.com>
2026-05-04 17:35:44 +0100

    feat: scheduler to ec2-status-check

--- ec2-status-check.py
+++ ec2-status-check.py
@@ -1,14 +1,20 @@
 import boto3
+import schedule
 
 client = boto3.client('ec2', region_name = "eu-north-1")
 
-response = client.describe_instance_status(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+def check_instance_status():
+    response = client.describe_instance_status(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    if response:
+        print("Status   ID")
+        for inst in response['InstanceStatuses']:
+            print(f"{inst['InstanceState']['Name']} {inst['InstanceId']}")
+    print('----------')
 
-if response:
-    print("Status   ID")
-    for inst in response['InstanceStatuses']:
-        print(f"{inst['InstanceState']['Name']} {inst['InstanceId']}")
 
-print('----------')
+schedule.every(5).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()
 
